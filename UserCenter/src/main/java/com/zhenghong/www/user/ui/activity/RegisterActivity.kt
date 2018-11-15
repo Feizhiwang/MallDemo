fb96df211c40109f119fe7b750111b5cf6c5aecd
Zheng Hong <zh_1991@163.com>
2018-11-15 16:57:51 +0800

    add UserCenter Modul

---
+++ UserCenter/src/main/java/com/zhenghong/www/user/ui/activity/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.zhenghong.www.user.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.zhenghong.www.user.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
