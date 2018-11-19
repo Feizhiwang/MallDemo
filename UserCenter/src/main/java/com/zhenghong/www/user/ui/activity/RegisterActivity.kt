8048ad02f1c88cdacc69c71eddaa38947857a88e
Zheng Hong <zh_1991@163.com>
2018-11-19 17:09:21 +0800

    LoginActivity

--- UserCenter/src/main/java/com/zhenghong/www/user/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/zhenghong/www/user/ui/activity/RegisterActivity.kt
@@ -3,11 +3,13 @@ package com.zhenghong.www.user.ui.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.zhenghong.www.user.R
+import org.jetbrains.anko.startActivity
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        startActivity<LoginActivity>("id" to 10);
     }
 }
